9f989008a77afc39f0648780100d12694b86d703
Víctor Mena Sánchez <vmenasanchez@gmail.com>
2025-10-14 09:45:05 +0200

    Usando la anotación @modelAttribute como método independiente de controlador

--- springboot-web/src/main/java/com/victor/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ springboot-web/src/main/java/com/victor/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,15 +1,21 @@
 package com.victor.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.victor.curso.springboot.webapp.springboot_web.models.User;
-
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
- * Un controlador es una clase común y corriente ya que maneja métodos del request.
+ * Un controlador es una clase común y corriente ya que maneja métodos del
+ * request.
  * 
  * Se conocen como métodos handler.
  * 
@@ -23,29 +29,32 @@ import com.victor.curso.springboot.webapp.springboot_web.models.User;
  * 
  * Tamnbién es una especialización de un componente.
  * 
- * En los return a la vista tengo que devolver el nombre de la plantilla html como un String.
+ * En los return a la vista tengo que devolver el nombre de la plantilla html
+ * como un String.
  * 
  * 
  */
 
- @Controller
+@Controller
 
 public class UserController {
-/**
- * 
- * Tengo que indicar que el método es un handler.
- * 
- * Es decir, un método que va a manejar una petición del request.
- * 
- * Un request tiene distintos tipos o métodos, se puede enviar un get, un post,un put, un delete...
- * 
- * Por ejemplo en este caso usaré @getmapping que es para cualquier petición que sea a través de una ruta.
- * 
- */
+    /**
+     * 
+     * Tengo que indicar que el método es un handler.
+     * 
+     * Es decir, un método que va a manejar una petición del request.
+     * 
+     * Un request tiene distintos tipos o métodos, se puede enviar un get, un
+     * post,un put, un delete...
+     * 
+     * Por ejemplo en este caso usaré @getmapping que es para cualquier petición que
+     * sea a través de una ruta.
+     * 
+     */
 
- @GetMapping("/details")
- 
-    public String details(Model model){
+    @GetMapping("/details")
+
+    public String details(Model model) {
 
         /*
          * Desde aquí paso datos a la vista.
@@ -54,33 +63,78 @@ public class UserController {
          * 
          * Tengo que pasarle e importar el parámetro model de spring.
          */
-        //Llamo a los parámetros del objeto user
+        // Llamo a los parámetros del objeto user
         User user = new User("Germán", "Doblas");
-         model.addAttribute("title", "HOLA MI GENTE");
+        model.addAttribute("title", "HOLA MI GENTE");
 
-         model.addAttribute("user", user);
+        user.setEmail("gerzor@carteras.com");
 
-         //model.addAttribute("lastname", "Mena");
+        model.addAttribute("user", user);
 
-        //Plantilla HTML
+        // model.addAttribute("lastname", "Mena");
+
+        // Plantilla HTML
         return "details";
 
     }
 
-/* 
-Se puede hacer con map de java.util
+    /*
+     * Se puede hacer con map de java.util
+     * 
+     * public String details(Map<String, Objet>model){
+     * 
+     * model.put("title", "HOLA MI GENTE");
+     * 
+     * model.put("name", "Víctor");
+     * 
+     * model.put("lastname", "Mena");
+     * 
+     * 
+     * return "details";
+     * 
+     * }
+     */
 
-    public String details(Map<String, Objet>model){
+    /*
+     * Voy a implementar un nuevo método en este controlador para crear listas en
+     * thymeleaf.
+     *
+     * Será una lista de usuarios que será reenderizada en la vista.
+     * 
+     * Lo primero será declarar un método del tipo getMapping, con la url
+     * específica.
+     */
 
-         model.put("title", "HOLA MI GENTE");
+    @GetMapping("/list")
+    public String list(ModelMap model) {
 
-         model.put("name", "Víctor");
+        // model.addAttribute("users", users);
 
-         model.put("lastname", "Mena");
+        model.addAttribute("title", "Listado de usuarios.");
 
-       
-        return "details";
+        return "list";
 
     }
-*/
+    
+/*
+ * @ModelAttribute es una anotación de Spring MVC que se usa para pasar datos entre el controlador y la vista (Thymeleaf).
+ * 
+ * En este caso lo estoy usando como un método del controlador para que su valor esté siempre disponible.
+ * 
+ * Es decir, podría reenderizar la tabla users en details también.
+ */
+
+@ModelAttribute("users")
+
+    public List<User> userModel() {
+
+        return Arrays.asList(
+                new User("Mario", "Gutierrez", "mario@corazones.com"),
+                new User("Cristian", "Sánchez", "cris@himenes.sex"),
+                new User("Gar", "Saxon", "mando@beskar.man"),
+                new User("Darth", "Vader"));
+
+        
+    }
+
 }
--- springboot-web/src/main/java/com/victor/curso/springboot/webapp/springboot_web/models/User.java
+++ springboot-web/src/main/java/com/victor/curso/springboot/webapp/springboot_web/models/User.java
@@ -6,17 +6,26 @@ public class User {
     //Atributos
 
     private String name;
-
+    
     private String lastname;
 
+    private String email;
 
-    //Genero un constructor con los atributos click izquierdo- source action - generate getters y setters
+
+
     
-    public User(String name, String lastname) {
+    //Genero un constructor con los atributos click izquierdo- source action - generate getters y setters
+
+    public User(String name, String lastname, String email) {
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+      public User(String name, String lastname) {
         this.name = name;
         this.lastname = lastname;
     }
-
 
 
     //Genero los getters y setters click izquierdo-source action- generate getters and setters
@@ -37,6 +46,15 @@ public class User {
         this.lastname = lastname;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
     
 
 }
